dca633b37fcb6ac8dedc3ba7e0d66d7390a0e654
Christopher McCoy <35251302+cmccoy13@users.noreply.github.com>
2018-02-13 17:52:06 -0800

    Update shader_fragmentSphere.glsl

--- resources/shader_fragmentSphere.glsl
+++ resources/shader_fragmentSphere.glsl
@@ -2,22 +2,22 @@
 out vec3 color;
 in vec3 vertex_normal;
 in vec3 vertex_pos;
-uniform vec3 campos;
+in vec4 gl_FragCoord;
+uniform vec4 Center;
+//uniform vec3 campos;
 void main()
 {
-vec3 n = normalize(vertex_normal);
-vec3 lp=vec3(100,10,100);
-vec3 ld = normalize(vertex_pos - lp);
-float diffuse = dot(n,ld);
-//color = vec3(1,0,0)*diffuse*0.7;
+	vec3 n = normalize(vertex_normal);
+	vec3 lp=vec3(100,10,100);
+	vec3 ld = normalize(vertex_pos - lp);
+	float diffuse = dot(n,ld);
 
-color = vertex_normal;
+	float dist = distance(gl_FragCoord, Center);
 
-vec3 cd = normalize(vertex_pos - campos);
-vec3 h = normalize(cd+ld);
-float spec = dot(n,h);
-spec = clamp(spec,0,1);
-spec = pow(spec,5);
-color += vec3(1,1,1)*spec*3;
-
+	color = vec3(0, 0, 0);
+	color += dist/180;
+	if(vertex_pos.z > 0.0)
+	{
+		discard;
+	}
 }
